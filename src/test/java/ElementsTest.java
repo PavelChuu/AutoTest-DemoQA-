371d0e782fec574a5aad0b4ff14a4a60d7559a32
PavelChuu <pavellchu@gmail.com>
2025-06-10 15:59:16 +0300

    Add TextBoxTest in ElementsTest.java

--- src/test/java/ElementsTest.java
+++ src/test/java/ElementsTest.java
@@ -1,2 +1,29 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+
 public class ElementsTest {
+    static WebDriver driver;
+    @BeforeAll
+    public static void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void TextBoxTest() {
+        driver.get("https://demoqa.com/text-box");
+        driver.findElement(By.id("userName")).click();
+        driver.findElement(By.id("userName")).sendKeys("UserName");
+        driver.findElement(By.id("userEmail")).click();
+        driver.findElement(By.id("userEmail")).sendKeys("UserEmail@gmail.com");
+        driver.findElement(By.id("currentAddress")).click();
+        driver.findElement(By.id("currentAddress")).sendKeys("CurrentAddress");
+        driver.findElement(By.id("permanentAddress")).click();
+        driver.findElement(By.id("permanentAddress")).sendKeys("PermanentAddress");
+        driver.findElement(By.id("submit")).click();
+    }
 }
