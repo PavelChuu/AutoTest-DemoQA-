091d133e04e3c3c80e7a2a169b6b3c88841cec51
PavelChuu <pavellchu@gmail.com>
2025-06-15 23:36:59 +0300

    Add Allure Report Add CheckBoxTest

--- src/test/java/ElementsTest.java
+++ src/test/java/ElementsTest.java
@@ -1,40 +1,42 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.time.Duration;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class ElementsTest {
-
-    static WebDriver driver;
     @BeforeAll
     public static void setUp() {
-        Configuration.browser = "firefox";}
-    @AfterEach
-    void tearDown() {
-        Selenide.closeWebDriver();}
+        Configuration.browser = "firefox";
+    }
 
     @Test
     public void TextBoxTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         open("https://demoqa.com/text-box");
+        WebDriverRunner.getWebDriver().manage().window().maximize();
+        Duration.ofSeconds(5);
         $("#userName").click();
         $("#userName").val("UserName");
-
         $("#userEmail").click();
         $("#userEmail").val("UserEmail@gmail.com");
-
         $("#currentAddress").click();
         $("#currentAddress").val("currentAddress");
-
         $("#permanentAddress").click();
         $("#permanentAddress").val("permanentAddress");
-
         $("#submit").click();
+    }
+
+    @Test
+    public void CheckBoxTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("https://demoqa.com/checkbox");
+        WebDriverRunner.getWebDriver().manage().window().maximize();
+        Duration.ofSeconds(5);
+        $(".rct-options > .rct-option-expand-all > .rct-icon-expand-all").click();
     }
 }
