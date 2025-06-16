c7e6556d3f4f6970ab76f93046c872b56ce9ef40
PavelChuu <pavellchu@gmail.com>
2025-06-16 18:49:20 +0300

    Update

--- src/test/java/ElementsTest.java
+++ src/test/java/ElementsTest.java
@@ -3,31 +3,32 @@ import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import java.time.Duration;
 import static com.codeborne.selenide.Selenide.*;
-public class ElementsTest {
 
-    @BeforeAll
-    public static void setUp() {
+
+public class ElementsTest {
+    @BeforeEach
+    public void setUp() {
         Configuration.browser = "firefox";
         Configuration.timeout = 600000;
+        open("https://demoqa.com");
+        WebDriverRunner.getWebDriver().manage().window().maximize();
     }
 
     @DisplayName("Test TextBox")
-    @RepeatedTest(5)
+    @Test
     public void TextBoxTest() {
         //Подключаем Faker
         Faker faker = new Faker();
         //Подключаем Логи Allure
         SelenideLogger.addListener("allure", new AllureSelenide());
         //Сам тест
-        open("https://demoqa.com/text-box");
-        WebDriverRunner.getWebDriver().manage().window().maximize();
-        Duration.ofSeconds(5);
+        $("svg[viewBox='0 0 448 512']").click();
+        $("#item-0 > svg[viewBox='0 0 1024 1024']").click();
         $("#userName").click();
         $("#userName").val(faker.name().firstName());
         $("#userEmail").click();
@@ -42,7 +43,6 @@ public class ElementsTest {
     @Test
     public void CheckBoxTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-        open("https://demoqa.com/checkbox");
         WebDriverRunner.getWebDriver().manage().window().maximize();
         Duration.ofSeconds(5);
         $(".rct-options > .rct-option-expand-all > .rct-icon-expand-all").click();
